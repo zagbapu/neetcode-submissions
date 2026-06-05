ed630495727d5b49f4ec7d705e86b24035b8c24d
Zikora Agbapu <69334935+zagbapu@users.noreply.github.com>
2026-06-05 13:42:44 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let max = 0;
+        let area = 0;
+        let l = 0;
+        let r = heights.length - 1;
+        while (l < r) {
+            let lh = heights[l];
+            let rh = heights[r];
+            const area = Math.min(lh,rh) * (r-l);
+            max = Math.max(max, area);
+            if (lh < rh) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return max;
+    }
+}
